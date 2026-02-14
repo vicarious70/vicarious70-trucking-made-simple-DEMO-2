a6dc878c5f121ac244df1acb9dca2c83ec1b42de
vicarious70 <vicarious70@gmail.com>
2026-02-14 07:52:09 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const SUPABASE_URL = "https://ggvfkeerwjnpgxhmatcv.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_sb_publishable_KEY_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_bJ8i_s76NqzLMkUb1uzGog_FyU3p16B";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
